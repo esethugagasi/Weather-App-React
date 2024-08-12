180e5cb0aeef1a0baaaab5e9229b2ec6c565bb7e
esethugagasi <esethugagasi14@gmail.com>
2024-08-12 14:28:08 +0200

    testing the code if its push correctly

--- src/Weather.js
+++ src/Weather.js
@@ -10,5 +10,5 @@ export default function Weather(props) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(handleResponse);
-  return <h2>Hello There</h2>;
+  return <h2>Hello There!</h2>;
 }
